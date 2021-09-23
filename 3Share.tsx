a41d8b49c55d5140d9af9c26f3f5d426f1cc20c2
timdonovanuk <8156439+timdonovanuk@users.noreply.github.com>
2021-09-23 20:41:45 +0100

    Delete 3Share.tsx

--- 3Share.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="3Share" tilewidth="32" tileheight="32" tilecount="580" columns="29">
- <image source="../../../Users/micro/Downloads/3Share-Logo.png" width="952" height="663"/>
-</tileset>
